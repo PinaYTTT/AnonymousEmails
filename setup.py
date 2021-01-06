c3ef919997e797171372451b4880ff21aef01717
PinaYT <pinacomandos@gmail.com>
2021-01-05 22:13:20 -0300

    AnonymousEmails
    Manda correos anonimos con AnonymousEmails Debes descargar la carpeta lib junto al exe para que funcione

--- setup.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup( name = "AnonymousEmails",
-        version = "1.0",
-        description = "Crea Emails anonimos para que nadie sepa que los mandaste",
-        icon = "Logo.ico",
-        executables = [Executable("Main.py")],)
